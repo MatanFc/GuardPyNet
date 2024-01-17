63da9391b746287ba93f7353baff8078da9068b1
MatanFc <matanf1992@gmail.com>
2024-01-17 13:02:57 +0200

    add env vars

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from github_helper import get_contributors
+from github_helper import get_contributors, get_github_link
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 
 
@@ -10,7 +12,7 @@ async def root():
 
 
 @app.get("/contributors/{package_name}")
-def get_package_contributors(package_name: str):
+async def get_package_contributors(package_name: str):
     contributors = get_contributors(package_name)
     return {
         "package": package_name,
